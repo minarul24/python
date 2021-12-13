ed531059681fd198fcdaefd2273ac291ae59dd85
Minarul I. Shawon <minarul@uvic.ca>
2021-12-13 13:42:19 -0800

    first of many exercises and beginner projects of python

--- beginnerProjects/madlib.py
+++ beginnerProjects/madlib.py
@@ -1,1 +1,9 @@
-print("Hello World")
+
+adjective = input("Please enter an Adjective for the madlib game: ")
+adjective_2 = input("Please, enter another adjective: ")
+noun = input("Please, enter a noun: ")
+verb = input("Please, enter a verb: ")
+adjective_3 = input("Please, enter another adjective: ")
+
+print(f"Wow, this is so {adjective}. I am so {adjective_2}. This is just a {noun} {verb}, that is {adjective_3}"\
+    f" language, PYTHON")
