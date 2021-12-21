9075af7dad7f826f20a3a9e4c867358f8e7c63ae
Minarul I. Shawon <minarul@uvic.ca>
2021-12-21 12:11:26 -0800

    rock paper scissor game played among user and computer

---
+++ beginnerProjects/rock_paper_scissor.py
@@ -0,0 +1,6 @@
+import random
+
+
+def game():
+    # user input is taken, whats their choice from rock, paper and scissor!
+    user_input = input("Choose 'r' for rock, 'p' for paper and 's' for scissor: \n")
