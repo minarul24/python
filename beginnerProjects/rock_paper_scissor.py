05677d003797b08a373b7ef01052de11240edefd
Minarul I. Shawon <minarul@uvic.ca>
2021-12-21 15:37:52 -0800

    complete method of game winner decision and the game played

--- beginnerProjects/rock_paper_scissor.py
+++ beginnerProjects/rock_paper_scissor.py
@@ -10,21 +10,28 @@ def game():
         # user input is taken, whats their choice from rock, paper and scissor!
         user_input = input("Choose 'r' for rock, 'p' for paper and 's' for scissor \n or choose 'q' to quit: \n")
         # takes computer input of the game
-        computer_input = 'r' #random.choice(['r', 'p', 's'])
-        game_decision(user_name,user_input,computer_input)
+        computer_input = random.choice(['r', 'p', 's'])
 
+        # calls the gam_decision method to decide who wins
+        game_decision(user_name, user_input, computer_input)
 
     print(f'{user_name}, thank you for playing!')
 
 
-def game_decision(username,user, computer):
+def game_decision(username, user, computer):
     if user == computer:
         print(f'{username}: {user} \nComputer: {computer} \nIt\'s a TIE!')
 
     # paper wins against rock, rock wins against scissors, scissor wins against paper
-    elif user == 'r' and computer == 's':
-        print(f'{username} WON!')
+    elif (user == 'r' and computer == 's') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
+        print(f'{username}: {user} \nComputer: {computer} \n{username} WON!')
+
+    elif (user == 's' and computer == 'r') or (user == 'p' and computer == 's') or (user == 'r' and computer == 'p'):
+        print(f'{username}: {user} \nComputer: {computer} \n{username}, you LOST!')
+
+    elif user == 'q':
+        print(f'Game Ends')
 
 
-
+# The function game() gets called to run the program
 game()
