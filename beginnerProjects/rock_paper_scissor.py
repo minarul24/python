cabb8d5fc176e61935e383da7c5baed216550bf1
Minarul I. Shawon <minarul@uvic.ca>
2021-12-21 12:46:24 -0800

    takes user input and computer generates a random character. Now, the program has only the tie condition

--- beginnerProjects/rock_paper_scissor.py
+++ beginnerProjects/rock_paper_scissor.py
@@ -2,5 +2,14 @@ import random
 
 
 def game():
+    print(f'Welcome to Rock, Paper and Scissor Game!')
     # user input is taken, whats their choice from rock, paper and scissor!
     user_input = input("Choose 'r' for rock, 'p' for paper and 's' for scissor: \n")
+    # takes computer input of the game
+    computer_input = random.choice(['r', 'p', 's'])
+
+    if user_input == computer_input:
+        print(f'User: {user_input} \nComputer: {computer_input} \nIt\'s a TIE!')
+
+
+game()
