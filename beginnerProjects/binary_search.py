919e7015431f287d43479677350abaac90894a34
Minarul I. Shawon <minarul@uvic.ca>
2022-07-11 11:15:15 -0700

    The method is defined and introduction is set

---
+++ beginnerProjects/binary_search.py
@@ -0,0 +1,10 @@
+"""
+Binary Search Algorithm:
+It uses the divide and conquer rule.
+
+"""
+
+# binary search method
+# a list and a target is passed as the arguments
+def binary_search(l, target):
+    
