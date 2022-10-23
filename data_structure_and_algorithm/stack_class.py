ca20a800fd505c548213cb631979bff2e4478bb5
Minarul I. Shawon <minarul@uvic.ca>
2022-10-23 00:24:51 -0700

    added the push method that inserts and item to the stack

--- data_structure_and_algorithm/stack_class.py
+++ data_structure_and_algorithm/stack_class.py
@@ -8,13 +8,23 @@ class stack:
     def size(self):
         return len(self.items)
 
-    
+    # method to insert an item to the stack
+    def push(self, item):
+        self.items.append(item)
 
 
 def main():
     s = stack()
+    # prints the size of the stack
     print(s.size())
 
+    # inserts an item to the stack
+    s.push(5)
+
+# prints the size of the stack
+    print(s.size())
+
+    print(s)
 
 main()
 
